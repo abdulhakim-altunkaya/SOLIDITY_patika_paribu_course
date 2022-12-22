0373f541b0668d595015731733ebcc7dc0963d23
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-12-22 23:16:32 +0300

    first 5 chapter notes compiled together

---
+++ 1_ValueData.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.7;
+
+contract ValueData {
+    //DATA TYPES: value and reference 
+    //VALUE TYPES: data stores a value
+    bool public a = false;
+    uint public b = 25;
+        // uint256:  2**256 - 1
+        // uint0:  2**8 - 1 
+        // uint16:  2**16 - 1
+    int public i = -2689;
+        //int256 -- 2**255 - 1
+    //To get the max and min integers
+    int public mintInt = type(int).min;
+    int public maxInt = type(int).max;
+    address public addr = address(5);
+    bytes32 public c;
+
+    //REFERENCE TYPES: they dont store an actual value, 
+    //they store a reference which points where the actual value is
+
+
+    //TYPES OF THE VARIABLES
+    //State variables: declared outside the functions, stored on the blockchain
+    //Local variables: declared inside the functions, not stored on the blockchain
+    //Global variables: blockchain related technical variables
+    //msg.sender (address), block.timestamp (uint), block.number(uint); 
+}

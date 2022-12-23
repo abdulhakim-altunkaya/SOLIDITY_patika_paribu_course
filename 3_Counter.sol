1b33a45f54e3d2a563b9b743ca36f72cbb29c74b
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-12-23 03:15:09 +0300

    counter file updated with comments

--- 3_Counter.sol
+++ 3_Counter.sol
@@ -1,16 +1,17 @@
-//SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.7;
+//SPDX-License-Identifier: MIT //License Version
+
+pragma solidity >=0.8.7; //compiler version
 
 contract Counter {
-    uint public count;
+    uint public count; //count variable, visibility is public
 
-    function increase() external {
-        count += 1;
+    function increase() external { //function to increase the value of above variable
+        count += 1; //increases by 1
     }
 
-    function decrease() external {
-        count -= 1;
+    function decrease() external { //function to decrease the value of above variable
+        count -= 1; //decreases by 1
     }
 
 

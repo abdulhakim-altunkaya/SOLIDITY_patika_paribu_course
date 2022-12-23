ab6792d8311b9fec59bff6ff6908f053f396d1af
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-12-23 23:06:17 +0300

    constructor, constant/immutable contract exercises finished

---
+++ 5_ConstantImmutable.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.7;
+
+contract ConstantImmutable {
+    uint public constant MYORDER = 8;
+
+}
+
+//Declaring a state variable as constant/immutable allows us to save gas
+//constant variables can never be updated
+//immutable variables can be updated with Constructor
+//constant variables are capital by convention
